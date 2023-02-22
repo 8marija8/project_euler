b4fcefd10345917a8e7aa20d4312116c3e9cc488
8marija8 <onecodingjourney@gmail.com>
2023-02-22 23:57:34 +0100

    fix

---
+++ problem_3/3.py
@@ -0,0 +1,64 @@
+
+"""
+
+prime_numbers = []
+x = -1
+
+while len(prime_numbers) != 10_001:
+
+  x += 1
+  prime_numbers.clear()
+
+  for i in range(x):
+
+    prime_numbers.append(1)
+
+    if  i %
+
+
+
+
+
+
+print(prime_numbers[10_000])
+
+"""
+
+
+
+y = 50
+
+primes = []
+
+for num in range(1, y+1):
+  if y / num == 1 or y / num == y: #  FIX THIS
+    primes.append(num)
+
+if len(primes) == 2:
+  print("prime number")
+  print(primes)
+else:
+  print("not prime number")
+  print(primes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
