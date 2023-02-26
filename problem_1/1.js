0c573c560c9d4c47d769a2351934e3efd49b20aa
marijaCode <90069037+marijaCode@users.noreply.github.com>
2023-02-26 12:24:59 +0100

    Update 1.js

--- problem_1/1.js
+++ problem_1/1.js
@@ -4,16 +4,6 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 
-*/
-
-let multiples = [];
-
-for (let i = 1; i < 1000; i++) {
-  if (i % 3 == 0 || i % 5 == 0) {
-    multiples.push(i)
-  }
-  
-}
 
 let total = 0
 
